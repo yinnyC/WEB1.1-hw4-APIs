c4aabd5284d926e75ab8da66a913433bbcb6da3f
Yin <yinnnn17@gmail.com>
2020-11-12 21:57:35 -0800

    Finished part2

--- app.py
+++ app.py
@@ -38,10 +38,12 @@ app.jinja_loader = my_loader
 
 pp = PrettyPrinter(indent=4)
 
-
+weekDays = ("Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday")
 ################################################################################
 # ROUTES
 ################################################################################
+
 
 @app.route('/')
 def home():
@@ -75,14 +77,6 @@ def results():
 
     # pp.pprint(result_json)
 
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the result_json object above.
-
-    # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()`
-    # function.
-    weekDays = ("Monday", "Tuesday", "Wednesday",
-                "Thursday", "Friday", "Saturday", "Sunday")
     context = {
         'date': datetime.now().strftime("%B %d, %Y"),
         'weekday': weekDays[datetime.now().weekday()],
@@ -101,16 +95,14 @@ def results():
 
 def get_min_temp(results):
     """Returns the minimum temp for the given hourly weather objects."""
-    # TODO: Fill in this function to return the minimum temperature from the
-    # hourly weather data.
-    pass
+    hourly_temp = [entry['temp'] for entry in results]
+    return max(hourly_temp)
 
 
 def get_max_temp(results):
     """Returns the maximum temp for the given hourly weather objects."""
-    # TODO: Fill in this function to return the maximum temperature from the
-    # hourly weather data.
-    pass
+    hourly_temp = [entry['temp'] for entry in results]
+    return min(hourly_temp)
 
 
 def get_lat_lon(city_name):
@@ -124,23 +116,20 @@ def get_lat_lon(city_name):
 @app.route('/historical_results')
 def historical_results():
     """Displays historical weather forecast for a given day."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
-    # parameters.
-    city = ''
-    date = '2020-08-26'
-    units = ''
+    city = request.args.get('city')
+    units = request.args.get('units')
+    date = request.args.get('date')
     date_obj = datetime.strptime(date, '%Y-%m-%d')
     date_in_seconds = date_obj.strftime('%s')
 
     latitude, longitude = get_lat_lon(city)
-
     url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
     params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # latitude, longitude, units, & date (in seconds).
-        # See the documentation here (scroll down to "Historical weather data"):
-        # https://openweathermap.org/api/one-call-api
-
+        'appid': API_KEY,
+        'units': units,
+        'lat': latitude,
+        'lon': longitude,
+        'dt': date_in_seconds,
     }
 
     result_json = requests.get(url, params=params).json()
@@ -151,17 +140,17 @@ def historical_results():
     result_current = result_json['current']
     result_hourly = result_json['hourly']
 
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the 'result_current' object above.
     context = {
-        'city': '',
+        'city': city,
         'date': date_obj,
+        'formated_date': date_obj.strftime("%B %d, %Y"),
+        'weekday': weekDays[date_obj.weekday()],
         'lat': latitude,
         'lon': longitude,
-        'units': '',
-        'units_letter': '',  # should be 'C', 'F', or 'K'
-        'description': '',
-        'temp': '',
+        'units': units,
+        'units_letter': get_letter_for_units(units),
+        'description': result_json['current']['weather'][0]['description'],
+        'temp': result_json['current']['temp'],
         'min_temp': get_min_temp(result_hourly),
         'max_temp': get_max_temp(result_hourly)
     }
